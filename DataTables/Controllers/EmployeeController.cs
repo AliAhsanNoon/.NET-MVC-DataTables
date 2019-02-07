d2db24d3632f5c5c7293dc19c633b366d2140044
Ali Ahsan Noon <alinoonciit@gmail.com>
2019-02-07 19:08:05 +0500

    CRUD Operations using DataTables

---
+++ DataTables/Controllers/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using DataTables.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DataTables.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        DataTableEntities _context = new DataTableEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EmployeeForm(int? id)
+        {
+                return View(_context.Employees.Where(x => x.EmpID == id).SingleOrDefault());   
+        }
+
+        [HttpPost]
+        public ActionResult EmployeeForm(Employee employee)
+        {
+            using (_context)
+            {
+                if (employee.EmpID == 0)
+                {
+                    _context.Employees.Add(employee);
+                    _context.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    _context.Entry(employee).State = EntityState.Modified;
+                    _context.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        public ActionResult GetData()
+        {
+            using (_context)
+            {
+                var empList = _context.Employees.ToList();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (_context)
+            {
+                var rmEmp = _context.Employees.Where(x=> x.EmpID == id).Single();
+                _context.Employees.Remove(rmEmp);
+                _context.SaveChanges();
+                return Json(new { success = true, message = "Successfully Deleted" }, JsonRequestBehavior.AllowGet);
+            }
+            
+        }
+    }
+}
